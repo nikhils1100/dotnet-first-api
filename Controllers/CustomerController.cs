e5700ef8c0fa586d287a3cd776e6c8ab22c40263
Nikhil <nikhil.sharma@datagrokr.com>
2021-02-18 15:53:49 +0530

    CSV R/W functions added

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -7,6 +7,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.Text;
+using CsvHelper.Configuration;
+using System.IO;
+using CsvHelper;
 
 namespace CRUD_NwDb.Controllers
 {
@@ -32,20 +37,24 @@ namespace CRUD_NwDb.Controllers
         [HttpGet("details/{CustomerId}")]
         public string Details(int CustomerId)
         {
-            Console.WriteLine(CustomerId);
-            if (_db.Customer.Find(CustomerId) != null)
-            {
-                var objList = _db.Customer.Find(CustomerId);
-                // var objList2 = (IEnumerable<Customer>)_db.Customer.Where(b=>b.CustomerId==CustomerId);
+            try{
+                if (_db.Customer.Find(CustomerId) != null)
+                {
+                    var objList = _db.Customer.Find(CustomerId);
+                    // var objList2 = (IEnumerable<Customer>)_db.Customer.Where(b=>b.CustomerId==CustomerId);
 
                 string obj_json = JsonConvert.SerializeObject(objList);
 
-                return obj_json; // Ok - Library for returning Json response
+                    return obj_json; 
+                }
+                else
+                {
+                    return "Not Found";
+                    //return NotFound();
+                }
             }
-            else
-            {
-                return "Not Found";
-                //return NotFound();
+            catch(Exception e){
+                throw;
             }
         }
 
@@ -147,7 +156,89 @@ namespace CRUD_NwDb.Controllers
             }
             catch
             {
-                return "Error";
+                throw;
+            }
+        }
+
+        // GET: CustomerController/Csv/get
+        [HttpGet("Csv/get")]
+        public IActionResult GetCsvData(){
+            try{
+                var path = "C:\\Users\\Nikhil\\Downloads\\Northwind_database_csv\\customer_custom.csv";
+
+                using (StreamReader reader = new StreamReader(path))
+                using (CsvReader csv_reader = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv_reader.GetRecords<Customer>().ToList();
+
+                    return Ok(records);
+                }
+            }
+            catch(Exception e){
+                throw;
+            }
+        }
+
+        // POST: CustomerController/Csv/post
+        [HttpPost("Csv/post")]
+        public IActionResult WriteCsvData(Customer customer){
+            try{
+                var path = "C:\\Users\\Nikhil\\Downloads\\Northwind_database_csv\\customer_custom.csv";
+
+                // Append to the file.
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    // Don't write the header again.
+                    HasHeaderRecord = false,
+                };
+                using(Stream stream = System.IO.File.Open(path, FileMode.Append))
+                using(StreamWriter sw = new StreamWriter(stream))
+                using(CsvWriter csv_writer = new CsvWriter(sw, config)){
+                    csv_writer.WriteRecord<Customer>(customer);
+
+                    return Ok(customer);
+                }
+            }
+            catch(Exception e){
+                throw;
+            }
+        }
+
+        // UPDATE: Conterollers/Csv/UpdateToDatabase
+        [HttpPost("Csv/CsvToDatabase")]
+        public IActionResult CsvToDatabase(string path){
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                using (CsvReader csv_reader = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var record = new Customer();
+                    var records = csv_reader.GetRecords<Customer>().ToList();
+
+                    foreach(var r in records)
+                    {
+                        Customer obj = new Customer();
+                        obj.Address = r.Address;
+                        obj.City = r.City;
+                        obj.CompanyName = r.CompanyName;
+                        obj.Country = r.Country;
+                        obj.Fax = r.Fax;
+                        obj.Phone = r.Phone;
+                        obj.PostalCode = r.PostalCode;
+                        obj.Region = r.Region;
+
+                        _db.Customer.Add(obj);
+                    }
+                    _db.SaveChanges();
+
+                    records = csv_reader.GetRecords<Customer>().ToList();
+                    return Ok(records);
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+                return NoContent();
             }
         }
     }
